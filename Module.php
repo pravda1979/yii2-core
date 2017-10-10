8e8797d7240ea735000e5c74272a8b68313317f1
PRAVDA <pravda.aleksei@gmail.com>
2017-10-10 13:57:06 +0300

    Initial commit

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace pravda1979\core;
+
+/**
+ * main module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
---
+++ components/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pravda1979\core\components;
+
+use yii\web\Controller as BaseController;
+
+/**
+ * Default controller for the `main` module
+ */
+class Controller extends BaseController
+{
+
+}
