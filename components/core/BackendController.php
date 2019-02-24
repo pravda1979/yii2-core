08a408fb469b2526fe3bdc355ed4559db7ae50ef
pravda <pravda.aleksei@gmail.com>
2019-02-25 00:50:34 +0300

    Fixed remember url ajax requests.

--- components/core/BackendController.php
+++ components/core/BackendController.php
@@ -44,7 +44,7 @@ class BackendController extends Controller
     public function afterAction($action, $result)
     {
         // Remember current url for goBack() function after create/update/delete record
-        if (!$action instanceof Action && !in_array($action->getUniqueId(), ['core/default/delete-cache', 'core/options/index', 'core/user/profile'])) {
+        if (!Yii::$app->request->isAjax && !$action instanceof Action && !in_array($action->getUniqueId(), ['core/default/delete-cache', 'core/options/index', 'core/user/profile'])) {
             Url::remember();
         }
 
